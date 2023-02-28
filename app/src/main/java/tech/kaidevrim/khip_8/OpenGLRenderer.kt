d29a31f392a89e4cf0d0e49488c833bd87d73c38
Kai Devrim <kai@devrim.tech>
2023-02-27 19:02:54 -0800

    chore: Add missing changes

--- app/src/main/java/tech/kaidevrim/khip_8/OpenGLRenderer.kt
+++ app/src/main/java/tech/kaidevrim/khip_8/OpenGLRenderer.kt
@@ -6,8 +6,10 @@ import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class OpenGLRenderer : GLSurfaceView.Renderer {
+    private lateinit var triangle: Triangle
     override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
         GLES20.glClearColor(1F, 0F, 0F, 1F)
+        triangle = Triangle()
     }
 
     override fun onSurfaceChanged(p0: GL10, width: Int, height: Int) {
@@ -16,5 +18,17 @@ class OpenGLRenderer : GLSurfaceView.Renderer {
 
     override fun onDrawFrame(p0: GL10?) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+    }
+
+    fun loadShader(type: Int, shaderCode: String): Int {
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        return GLES20.glCreateShader(type).also { shader ->
+
+            // add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode)
+            GLES20.glCompileShader(shader)
+        }
     }
 }
---
+++ app/src/main/java/tech/kaidevrim/khip_8/Triangle.kt
@@ -0,0 +1,77 @@
+package tech.kaidevrim.khip_8
+
+import android.opengl.GLES20
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+
+const val COORDS_PER_VERTEX = 3
+var triangleCoords = floatArrayOf(     // in counterclockwise order:
+    0.0f, 0.622008459f, 0.0f,      // top
+    -0.5f, -0.311004243f, 0.0f,    // bottom left
+    0.5f, -0.311004243f, 0.0f      // bottom right
+)
+
+class Triangle {
+    private val vertexShaderCode =
+        "attribute vec4 vPosition;" +
+                "void main() {" +
+                "  gl_Position = vPosition;" +
+                "}"
+
+    private val fragmentShaderCode =
+        "precision mediump float;" +
+                "uniform vec4 vColor;" +
+                "void main() {" +
+                "  gl_FragColor = vColor;" +
+                "}"
+
+    // Set color with red, green, blue and alpha (opacity) values
+    val color = floatArrayOf(0.63671875f, 0.76953125f, 0.22265625f, 1.0f)
+
+    private var vertexBuffer: FloatBuffer =
+        // (number of coordinate values * 4 bytes per float)
+        ByteBuffer.allocateDirect(triangleCoords.size * 4).run {
+            // use the device hardware's native byte order
+            order(ByteOrder.nativeOrder())
+
+            // create a floating point buffer from the ByteBuffer
+            asFloatBuffer().apply {
+                // add the coordinates to the FloatBuffer
+                put(triangleCoords)
+                // set the buffer to read the first coordinate
+                position(0)
+            }
+        }
+    private var mProgram: Int
+
+    init {
+        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+        val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+
+        // create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram().also {
+
+            // add the vertex shader to program
+            GLES20.glAttachShader(it, vertexShader)
+
+            // add the fragment shader to program
+            GLES20.glAttachShader(it, fragmentShader)
+
+            // creates OpenGL ES program executables
+            GLES20.glLinkProgram(it)
+        }
+    }
+
+    fun loadShader(type: Int, shaderCode: String): Int {
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        return GLES20.glCreateShader(type).also { shader ->
+
+            // add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode)
+            GLES20.glCompileShader(shader)
+        }
+    }
+}
