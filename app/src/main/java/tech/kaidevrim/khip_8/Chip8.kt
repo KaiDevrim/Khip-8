7d2e725b0f9ebd4b38a3b987501d5d704058b781
Kai Devrim <kai@devrim.tech>
2023-03-07 18:32:26 -0800

    Remove null from all variables and convert instructions to functions

--- app/src/main/java/tech/kaidevrim/khip_8/Chip8.kt
+++ app/src/main/java/tech/kaidevrim/khip_8/Chip8.kt
@@ -1,30 +1,31 @@
-@file:OptIn(ExperimentalUnsignedTypes::class, ExperimentalUnsignedTypes::class, ExperimentalUnsignedTypes::class)
+@file:OptIn(ExperimentalUnsignedTypes::class, ExperimentalUnsignedTypes::class)
+
 package tech.kaidevrim.khip_8
 
 import kotlinx.datetime.Clock
-import java.lang.Exception
-import kotlin.random.Random
-import kotlin.random.nextUInt
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
 import kotlin.math.floor
+import kotlin.random.Random
+import kotlin.random.nextUInt
 
 
 class Chip8 {
-    private var drawer: Drawer? = null
-    private val scale: Double = 10.0
-    private var opcode: UShort? = null
+    private lateinit var drawer: Drawer
+    private var scale: Double = 10.0
+
+    private var opcode: UShort = 0u
     private var memory: UByteArray = UByteArray(4096)
     private var graphics: UByteArray = UByteArray(64 * 32)
     private var registers: UByteArray = UByteArray(16)
-    private var registerIndex: UShort? = null
-    private var programCounter: UShort? = null
+    private var registerIndex: UShort = 0u
+    private var programCounter: UShort = 0x200u
 
-    private var delayTimer: UByte? = null
-    private var soundTimer: UByte? = null
+    private var delayTimer: UByte = 0u
+    private var soundTimer: UByte = 0u
 
     private var stack: UShortArray = UShortArray(16)
-    private var sp: UShort? = null
+    private var sp: UShort = 0u
 
     private val chip8Fontset: IntArray = intArrayOf(
         0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -46,17 +47,21 @@ class Chip8 {
     )
 
     private var keys: UByteArray = UByteArray(16)
-    fun randomUInt() = Random(Clock.System.now().epochSeconds).nextUInt()
+
+    private fun randomUInt() = Random(Clock.System.now().epochSeconds).nextUInt()
     private fun shiftLeft(uShort: UShort, bits: Int): UShort {
         return (uShort.toInt() shl bits).toUShort()
     }
-    fun shiftRight(uShort: UShort, bits: Int): UShort {
+
+    private fun shiftRight(uShort: UShort, bits: Int): UShort {
         return (uShort.toInt() shr bits).toUShort()
-     }
+    }
+
     @OptIn(ExperimentalUnsignedTypes::class)
     fun init(self: Chip8, drawerIn: Drawer) {
         self.drawer = drawerIn
-        self.programCounter = 512u
+        self.scale = 10.0
+        self.programCounter = 0x200u
         self.opcode = 0u
         self.registerIndex = 0u
         self.sp = 0u
@@ -70,7 +75,7 @@ class Chip8 {
         self.delayTimer = 0u
         self.soundTimer = 0u
 
-        self.chip8Fontset.forEachIndexed {index, element -> self.memory[index] = element.toUByte() }
+        self.chip8Fontset.forEachIndexed { index, element -> self.memory[index] = element.toUByte() }
 
         self.graphics[5] = 1u
         self.graphics[1] = 1u
@@ -78,43 +83,61 @@ class Chip8 {
     }
 
     private fun incrementPc(self: Chip8) {
-        self.programCounter = (self.programCounter?.plus(2u))?.toUShort()
+        self.programCounter = (self.programCounter + 2u).toUShort()
+    }
+
+    fun cycle(self: Chip8) {
+        setDisplay(self)
+        getOpcode(self)
+        decode(self)
     }
 
     @OptIn(ExperimentalUnsignedTypes::class)
-    fun cycle(self: Chip8) {
-        if (self.programCounter!! > 4095u) {
-            throw Exception("OPcode out of range! Your program has an error!")
+    private fun getOpcode(self: Chip8) {
+        if (self.programCounter > 0xFFFu) {
+            throw Exception("opcode out of range! Your program has an error!")
         }
-        setDisplay(self)
-        shiftRight(0u, 0)
-        shiftLeft(0u, 0)
-        randomUInt()
-        self.opcode = self.memory[self.programCounter?.toInt()!!].toUShort()
-        self.opcode = shiftLeft(self.opcode!!, 8).or(self.memory[(self.programCounter!! + 1u).toInt()].toUShort())
+        self.opcode = self.memory[self.programCounter.toInt()].toUShort()
+        self.opcode = shiftLeft(self.opcode, 8).or(self.memory[(self.programCounter + 1u).toInt()].toUShort())
+    }
 
-        when(opcode?.and(4096u)) {
-            224u.toUShort() -> {
+    private fun spAdd(self: Chip8, by: UInt): Int {
+        return (self.sp + by).toInt()
+    }
+
+    private fun spMinus(self: Chip8, by: UInt): Int {
+        return (self.sp - by).toInt()
+    }
+
+    @OptIn(ExperimentalUnsignedTypes::class)
+    private fun decode(self: Chip8) {
+        var x: Int = 0
+
+        // Exact instructions
+        when (opcode.toUInt()) {
+            // Clear Screen
+            0x00E0u -> {
                 self.graphics.forEachIndexed { index, _ -> self.graphics[index] = 0u }
                 self.incrementPc(self)
             }
 
-            238u.toUShort() -> {
-                self.sp = (self.sp!! - 1u).toUShort()
-                self.programCounter = self.stack[self.sp?.toInt()!!]
+            // Return from subroutine
+            0x00EEu -> {
+                self.sp = (self.sp - 1u).toUShort()
+                self.programCounter = self.stack[self.sp.toInt()]
                 self.incrementPc(self)
             }
+        }
 
-            32768u.toUShort() -> {
-                when(opcode?.and(15u)) {
-                    0u.toUShort() -> {
-                    }
-                }
+        // Access data in opcode
+        when (opcode.and((0xF000u)).toUInt()) {
+            // 1NNN Jump to location nnn
+            0x1000u -> {
+                programCounter = self.opcode.and(0x0FFFu)
             }
 
-            else -> {
-                // var first = shiftRight(self.opcode!!, 12)
-                println("The current opcode is: " + self.opcode)
+            0x2000u -> {
+
             }
         }
     }
@@ -123,15 +146,14 @@ class Chip8 {
     private fun setDisplay(self: Chip8) {
         for (i in self.graphics.indices) {
             if (self.graphics[i] == 0u.toUByte()) {
-                self.drawer?.fill = ColorRGBa.BLACK
-            }
-            else {
-                self.drawer?.fill = ColorRGBa.WHITE
+                self.drawer.fill = ColorRGBa.BLACK
+            } else {
+                self.drawer.fill = ColorRGBa.WHITE
             }
             val x: Int = (i % 64)
             val y: Int = floor(i / 64.0).toInt()
 
-            self.drawer?.rectangle(x*scale, y*scale, scale, scale)
+            self.drawer.rectangle(x * scale, y * scale, scale, scale)
         }
     }
 }
